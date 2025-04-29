698a13781ffb3698174004350bfbef229841dc03
Iakuba <diman.150106@gmail.com>
2025-04-29 19:23:41 +0300

    loading text changed

--- customizer/src/app/components/CustomCanvas/CustomLoader.tsx
+++ customizer/src/app/components/CustomCanvas/CustomLoader.tsx
@@ -1,18 +1,13 @@
 import React from "react";
-import {Box, useProgress, Html} from "@react-three/drei";
+import {TorusKnot, useProgress, Html} from "@react-three/drei";
 
 const CustomLoader= () => {
     const { progress } = useProgress();
     return (
         // @ts-ignore
         <group>
-            <Box args={[1, 1, 1]} position={[0,0,0]}>
-                {/* @ts-ignore */}
-                <meshStandardMaterial wireframe color="grey" opacity={0.5} transparent />
-            </Box>
-            {/* @ts-ignore */}
             <Html center>
-                <div style={{ color: 'white' }}>
+                <div style={{ color: '#ffab44', width: '200 px' }} className="text-3xl">
                     Завантаження: {progress.toFixed(0)}%
                 </div>
             </Html>
