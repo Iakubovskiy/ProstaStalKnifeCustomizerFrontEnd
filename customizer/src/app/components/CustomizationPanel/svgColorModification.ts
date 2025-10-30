6bfab741b140039ab3787fac9d6ec2745b4202c6
Iakuba <diman.150106@gmail.com>
2025-10-30 02:20:01 +0200

    replace color fix, hope

--- customizer/src/app/components/CustomizationPanel/svgColorModification.ts
+++ customizer/src/app/components/CustomizationPanel/svgColorModification.ts
@@ -6,17 +6,19 @@ export const replaceSvgColor = (svgText: string, newColor: string): string => {
             return `${p1}stroke:${newColor}${p3}"`;
         }
     );
-    modifiedSvg = modifiedSvg.replace(/(<(path|g|svg)[^>]*)fill\s*=\s*"#[0-9a-fA-F]{3,6}"([^"]*)"/gi,
-        (match, p1, tag, p3) => {
-            return `${p1}fill="${newColor}"${p3}"`;
-        }
+    modifiedSvg = modifiedSvg.replace(
+        /(stroke|fill)\s*:\s*(?!none)(#[0-9a-fA-F]{3,6}|[a-zA-Z]+)/gi,
+        (match, prop) => `${prop}:${newColor}`
     );
 
     modifiedSvg = modifiedSvg.replace(
-        /(fill|stroke)\s*:\s*(?!none)(#[0-9a-fA-F]{3,6})/gi,
-        (match, prop, oldColor) => {
-            return `${prop}:${newColor}`;
-        }
+        /(<[^>]*style="[^"]*)(stroke|fill)\s*:\s*(?!none)(#[0-9a-fA-F]{3,6}|[a-zA-Z]+)([^"]*)"/gi,
+        (match, p1, prop, oldColor, p4) => `${p1}${prop}:${newColor}${p4}"`
+    );
+
+    modifiedSvg = modifiedSvg.replace(
+        /(<[^>]*?)\s*(stroke|fill)\s*=\s*"(?!none)(#[0-9a-fA-F]{3,6}|[a-zA-Z]+)"([^>]*>)/gi,
+        (match, p1, prop, oldColor, p4) => `${p1} ${prop}="${newColor}"${p4}`
     );
 
     return modifiedSvg;
